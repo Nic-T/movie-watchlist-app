c8f3ec4e7680be9892c7bce84dca86b98e12c05c
Nic-T <nicolastantau2003@gmail.com>
2021-06-04 01:25:01 +0300

    added a grid layout for the movies

--- client/src/pages/Movies.js
+++ client/src/pages/Movies.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
 import axios from 'axios';
-
-
+import Grid from '@material-ui/core/Grid';
+import {Container} from '@material-ui/core';
+import MovieCard from '../components/MovieCard';
 
 function Movies(){
 
-    const getMovies = () =>{
-        axios.get("http://localhost:5000/movies")
+
+    const [movies, setMovies] = useState([]);
+    const [check, setCheck] = useState(false)
+
+    
+
+    const getMovies = async () =>{
+       await axios.get("http://localhost:5000/movies")
             .then((res) =>{ 
-                console.log(res);
+                setMovies(res.data.results)
+                setCheck(true)
                 
         })
     }
 
+    useEffect(() =>{
+
+        getMovies();
+
+    }, []);
+
     return (
         <div>
-            <button onClick={getMovies}> Get Joke </button>
+           <button onClick={()=>console.log(movies)}> Click</button>
+           <Grid
+            container
+            direction="row"
+            justify="space-around"
+            
+            >
+                
+                {movies.map(movie => 
+                   
+                   
+                <Grid item key={movie.id} xs={12} md={6} lg={3} >
+                    <MovieCard movie={movie}/>
+                </Grid>
+                    
+                        
+                )}
+           </Grid>
+            
         </div>
     )
 }
