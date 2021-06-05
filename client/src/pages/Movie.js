ad5e9cdd80da8aad6de5da374197ac141cc83915
Nic-T <nicolastantau2003@gmail.com>
2021-06-05 22:46:20 +0300

    this is the movie page for information about individual movies

---
+++ client/src/pages/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Movie(props) {
+
+    
+
+
+    console.log(props)
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
+
+export default Movie
+
