f06f772bdf0a76d1813599091f631d5d3a638457
Nic-T <nicolastantau2003@gmail.com>
2021-06-19 18:50:37 +0300

    added a new route to the server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const moviesRouter = require("./routes/movies");
+const movieRouter = require("./routes/movie");
 const cors = require('cors');
 const app = express();
 
 app.use(cors())
 
 app.use('/', moviesRouter);
-
+app.use('/', movieRouter);
 
 
 
