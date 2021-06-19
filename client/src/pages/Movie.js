e66354ed1875706e119530155d1ec8f880e9a56d
Nic-T <nicolastantau2003@gmail.com>
2021-06-19 18:56:59 +0300

    created the axios request to request data from backend

--- client/src/pages/Movie.js
+++ client/src/pages/Movie.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState,useEffect,Fragment} from 'react'
+import axios from 'axios';
+import {useParams} from "react-router-dom";
 
-function Movie(props) {
+
+
+function Movie() {
+    
+    const {id} = useParams();
+
+    const [movie, setMovie] = useState([]);
+    const [check, setCheck]= useState(false)
+
+    const getMovie = async () =>{
+        await axios.get(`http://localhost:5000/movies/${id}`)
+            .then((res) =>{ 
+                setMovie(res.data.results)
+                setCheck(true)
+                
+                 
+        })
+    }
+    useEffect(() =>{
+
+        getMovie();
+
+    }, []);
 
     
-
-
-    console.log(props)
     return (
         <div>
             <h1></h1>
