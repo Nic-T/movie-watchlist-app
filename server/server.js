24c47713e97d0c3c93e9246ac67ba6e76dd84b58
Nic-T <nicolastantau2003@gmail.com>
2021-06-28 21:42:50 +0300

    added new routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const moviesRouter = require("./routes/movies");
 const movieRouter = require("./routes/movie");
-const creditsRouter = require("./routes/credits")
+const creditsRouter = require("./routes/credits");
+const imageRouter = require("./routes/images");
+const recRouter = require("./routes/recommendations");
 const cors = require('cors');
 const app = express();
 
@@ -10,7 +12,8 @@ app.use(cors())
 app.use('/', moviesRouter);
 app.use('/', movieRouter);
 app.use('/', creditsRouter);
-
+app.use('/', imageRouter);
+app.use('/', recRouter);
 
 
 app.listen(5000, err => {
