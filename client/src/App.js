9aa6cc179b11c9b6d0fa4f545dde5a9b6d34b908
Nic-T <nicolastantau2003@gmail.com>
2021-07-13 18:13:31 +0300

    added the Layout component around the main page

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,19 @@ import Movies from './pages/Movies'
 import Favorites from './pages/Favorites'
 import Shows from './pages/Shows'
 import Movie from './pages/Movie'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route  path="/movies" exact component={Movies}/>
-        <Redirect from="/" exact to="/movies"/>
-        <Route path="/movies/:id" component={Movie} />
-        <Route  path="/favorites" component={Favorites}/>
-        <Route  path="/shows" component={Shows}/>
+        <Layout>
+          <Route  path="/movies" exact component={Movies}/>
+          <Redirect from="/" exact to="/movies"/>
+          <Route path="/movies/:id" component={Movie} />
+          <Route  path="/favorites" component={Favorites}/>
+          <Route  path="/shows" component={Shows}/>
+        </Layout>
       </Switch>
 
     </Router>
