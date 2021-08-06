de35fa7b02c5830f481ab561be8e3d4b9e8de170
Nic-T <nicolastantau2003@gmail.com>
2021-08-06 21:38:27 +0300

    deleted the credits and created a separate component for them

--- client/src/pages/Movie.js
+++ client/src/pages/Movie.js
@@ -2,7 +2,9 @@ import React, {useState,useEffect,Fragment} from 'react'
 import axios from 'axios';
 import {useParams} from "react-router-dom";
 import YouTube from 'react-youtube';
-
+import styles from '../styles/Movie.module.css'
+import Carousel from 'react-material-ui-carousel'
+import Credits from '../components/Credits'
 
 function Movie() {
     
@@ -10,14 +12,14 @@ function Movie() {
 
     const [movie, setMovie] = useState([]);
     const [check, setCheck] = useState(false);
-    const [credits, setCredits] = useState([]);
+    
     const [images, setImages] = useState([]);
     const [rec, setRec] = useState([]);
     const [similar, setSimilar] =useState([]);
     const [release, setRelease] = useState([]);
     const [video, setVideos] = useState([])
     const imagePrefix ='https://image.tmdb.org/t/p/w500'
-    const videoPrefix ='https://www.youtube.com/watch?v='
+    
 
     const getMovie = async () =>{
         await axios.get(`http://localhost:5000/movie/${id}`)
@@ -26,12 +28,7 @@ function Movie() {
         })
     }
 
-    const getCredits = async () =>{
-        await axios.get(`http://localhost:5000/movie/${id}/credits`)
-            .then((res) =>{
-                setCredits(res.data)
-        })
-    }
+    
 
     const getImages = async () =>{
         await axios.get(`http://localhost:5000/movie/${id}/images`)
@@ -69,15 +66,16 @@ function Movie() {
 
     useEffect(() =>{
         getMovie();
-        getCredits();
+        
         getImages();
         getRec();
         getSimilar();
         getRelease();
         getVideos();
     }, []);
-    
-    if(!video.results){
+
+
+    if(!video.results && !movie){
         return(
         <div>
 
@@ -87,34 +85,13 @@ function Movie() {
         return (
             <div>
                 <h1>{movie.title}</h1>
-                {movie.genres.map(genre=>
-                    <p>{genre.name}</p>
-                )}
                 <p>{movie.overview}</p>
 
-                <YouTube videoId={`${video.results[0].key}`}/>
+                
                 
                 <img src={imagePrefix+movie.poster_path} height="562.5" width="375"/>
 
-                {credits.cast.map(person =>
-                    
-                    <div>
-                        <p>{person.name}</p>
-                        <p>{person.character} </p> 
-                        <p>{person.known_for_department}</p> 
-                        <img src={imagePrefix+person.profile_path} height="375" width="250" />
-                    </div>
-                )}
-
-                {credits.crew.map(person =>
-                    
-                    <div>
-                        <p>{person.name}</p>
-                        <p>{person.character} </p> 
-                        <p>{person.known_for_department}</p> 
-                        <img src={imagePrefix+person.profile_path} height="375" width="250" />
-                    </div>
-                )}
+                <Credits/>
 
             </div>
         )
